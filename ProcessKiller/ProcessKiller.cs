e6d2d81bb72c44576ecf0cb063cc1f634bdecdf6
damiralmaev <daralmaeva@yandex.ru>
2019-05-25 14:05:07 +0400

    Просто описание и залитие библиотеки

---
+++ ProcessKiller/ProcessKiller.cs
@@ -0,0 +1,43 @@
+﻿//Версия библиотеки 1.0
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//что я добавил
+using System.Diagnostics;
+
+namespace ProcessKiller
+{
+    public class Proces
+    {
+
+        //убить процесс
+
+        public static void Kill(string name)
+        {
+
+            Process[] proces = Process.GetProcesses();//получаем все процессы
+
+            foreach (Process nameprocess in proces)//подбираем все процессы
+            {
+                try
+                {
+                    if (name == nameprocess.ProcessName)//если нашли, то
+                    {
+                        nameprocess.Kill();//убиваем его ;)
+                    }
+                    else
+                    {
+
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
